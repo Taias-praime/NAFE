2377df51628c7aa27e260dd3bd01ce42eda7eaf6
kold <kold@Samuels-MacBook-Pro.local>
2024-05-23 11:06:51 +0000

    chore: updated COAS file download on click

--- src/components/ui-custom/files.tsx
+++ src/components/ui-custom/files.tsx
@@ -1,22 +1,24 @@
-import { Paperclip } from "lucide-react"
+import { Paperclip } from 'lucide-react';
 
-export const FilesList = ({files} : any) => {
-    return (
-        <ul className="flex flex-col gap-4">
-            {
-                files.map((f: any) => <FileItem file={f} /> )
-            }
-        </ul>
-    )
-}
+export const FilesList = ({ files }: any) => {
+	return (
+		<ul className="flex flex-col gap-4">
+			{files.map((f: any) => (
+				<FileItem file={f} />
+			))}
+		</ul>
+	);
+};
 
-export const FileItem = ({file}: any) => {
-    return (
-        <li className="py-3 px-5 bg-muted grid grid-cols-[30px_1fr] items-center gap-3 rounded-lg overflow-hidden min-h-16">
-            <div className="text-center">
-                <Paperclip className="text-foreground" size={25} />
-            </div>
-            <p className="line-clamp-2"> {file} </p>
-        </li>
-    )
-}
+export const FileItem = ({ file }: any) => {
+	return (
+		<a href={file} target="_blank">
+			<li className="py-3 px-5 bg-muted grid grid-cols-[30px_1fr] items-center gap-3 rounded-lg overflow-hidden min-h-16">
+				<div className="text-center">
+					<Paperclip className="text-foreground" size={25} />
+				</div>
+				<p className="line-clamp-2">{file}</p>
+			</li>
+		</a>
+	);
+};
